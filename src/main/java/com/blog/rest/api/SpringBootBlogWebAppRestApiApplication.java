954ce9954e7bd976ca3fbde83198007b56c2b9e7
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-12 13:58:00 +0700

    initial commit

---
+++ src/main/java/com/blog/rest/api/SpringBootBlogWebAppRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.blog.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogWebAppRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlogWebAppRestApiApplication.class, args);
+	}
+
+}
