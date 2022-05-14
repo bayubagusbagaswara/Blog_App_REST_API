202831670a4f21f5cacf72fa42ff87116e9e0b2a
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-14 18:22:12 +0700

    added entity class Role

---
+++ src/main/java/com/blog/rest/api/entity/role/Role.java
@@ -0,0 +1,28 @@
+package com.blog.rest.api.entity.role;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(name = "name")
+    private RoleName name;
+
+    // constructor with paramater RoleName
+    public Role(RoleName name) {
+        this.name = name;
+    }
+}
