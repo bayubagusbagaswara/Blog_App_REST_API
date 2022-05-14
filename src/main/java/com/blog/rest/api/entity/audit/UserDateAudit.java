08497e7ef179120f4f94db4ecd925abc0726f975
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-14 15:57:27 +0700

    added abstract class UserDateAudit

---
+++ src/main/java/com/blog/rest/api/entity/audit/UserDateAudit.java
@@ -0,0 +1,29 @@
+package com.blog.rest.api.entity.audit;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@EqualsAndHashCode(callSuper = true)
+@MappedSuperclass
+@Data
+@JsonIgnoreProperties(
+        value = { "createdBy", "updatedBy" },
+        allowGetters = true
+)
+public abstract class UserDateAudit extends DateAudit {
+
+    private static final long serialVersionUID = 1L;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String updatedBy;
+}
