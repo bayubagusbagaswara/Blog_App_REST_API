e6e1893e38f9c85d2c33e92af17b289e86e9b202
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-14 22:04:52 +0700

    added entity class User

---
+++ src/main/java/com/blog/rest/api/entity/user/User.java
@@ -0,0 +1,74 @@
+package com.blog.rest.api.entity.user;
+
+import com.blog.rest.api.entity.audit.DateAudit;
+import com.blog.rest.api.entity.role.Role;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = { "username" }),
+        @UniqueConstraint(columnNames = { "email" })
+})
+public class User extends DateAudit {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotBlank
+    @Column(name = "first_name")
+    @Size(max = 40)
+    private String firstName;
+
+    @NotBlank
+    @Column(name = "last_name")
+    @Size(max = 40)
+    private String lastName;
+
+    @NotBlank
+    @Column(name = "username")
+    @Size(max = 15)
+    private String username;
+
+    @NotBlank
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Size(max = 100)
+    @Column(name = "password")
+    private String password;
+
+    @NotBlank
+    @NaturalId
+    @Size(max = 40)
+    @Column(name = "email")
+    @Email
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "website")
+    private String website;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "fk_user_role_user_id"), referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", foreignKey = @ForeignKey(name = "fk_user_role_roke_id"), referencedColumnName = "id"))
+    private Set<Role> roles = new HashSet<>();
+}
