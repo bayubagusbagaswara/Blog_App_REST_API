e01fb61f550ae46b4c6339e2eb2f03a0c973b087
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-14 15:57:11 +0700

    added abstract class DateAudit

---
+++ src/main/java/com/blog/rest/api/entity/audit/DateAudit.java
@@ -0,0 +1,33 @@
+package com.blog.rest.api.entity.audit;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = { "createdAt", "updatedAt" },
+        allowGetters = true
+)
+public abstract class DateAudit implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
