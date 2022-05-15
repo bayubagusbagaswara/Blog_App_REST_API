c98cf82c8d7b191fca021e4e9048141015992f0e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-15 18:08:53 +0700

    added interface UserRepository

---
+++ src/main/java/com/blog/rest/api/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.blog.rest.api.repository;
+
+import com.blog.rest.api.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+}
