9e424f6679fe57c886ddaeb0193f2e24fb52e699
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-17 09:07:22 +0700

    added payload class ApiResponse

---
+++ src/main/java/com/blog/rest/api/payload/response/ApiResponse.java
@@ -0,0 +1,41 @@
+package com.blog.rest.api.payload.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@JsonPropertyOrder({
+        "success",
+        "message"
+})
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse implements Serializable {
+
+    @JsonIgnore
+    @Serial
+    private static final long serialVersionUID = 7702134516418120340L;
+
+    @JsonProperty("success")
+    private Boolean success;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonIgnore
+    private HttpStatus status;
+
+    public ApiResponse(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+}
