3cfde7702756af141d8810ca8d1f683343d9d717
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-17 09:00:19 +0700

    added method checkUsernameAvailability in UserService

--- src/main/java/com/blog/rest/api/service/UserService.java
+++ src/main/java/com/blog/rest/api/service/UserService.java
@@ -1,5 +1,6 @@
 package com.blog.rest.api.service;
 
+import com.blog.rest.api.payload.user.UserIdentityAvailability;
 import com.blog.rest.api.payload.user.UserSummary;
 import com.blog.rest.api.security.UserPrincipal;
 
@@ -7,4 +8,7 @@ public interface UserService {
 
     // ambil current user yang sedang mengakses aplikasi kita
     UserSummary getCurrentUser(UserPrincipal currentUser);
+
+    // cek apakah username sudah tersedia atau belum
+    UserIdentityAvailability checkUsernameAvailability(String username);
 }
