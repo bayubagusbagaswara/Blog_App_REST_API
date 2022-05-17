fe01c5555108ca1dee1999af1848c634bbddb9a6
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-17 07:51:42 +0700

    added interface CategoryRepository

---
+++ src/main/java/com/blog/rest/api/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.blog.rest.api.repository;
+
+import com.blog.rest.api.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
