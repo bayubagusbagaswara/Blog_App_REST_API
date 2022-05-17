af416dbd7579db152f8086fc2f0616345a981fcb
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-17 09:39:41 +0700

    added annotation @Serial

--- src/main/java/com/blog/rest/api/entity/Album.java
+++ src/main/java/com/blog/rest/api/entity/Album.java
@@ -8,6 +8,7 @@ import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.io.Serial;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.List;
 @Data
 public class Album extends UserDateAudit {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
