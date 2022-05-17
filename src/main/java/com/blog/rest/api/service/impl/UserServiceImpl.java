0f090fd49fb2597fa4b70486c0172a73e40b7cc9
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-17 16:21:37 +0700

    implement method getUserProfile

--- src/main/java/com/blog/rest/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/blog/rest/api/service/impl/UserServiceImpl.java
@@ -1,20 +1,30 @@
 package com.blog.rest.api.service.impl;
 
+import com.blog.rest.api.entity.user.User;
 import com.blog.rest.api.payload.user.UserIdentityAvailability;
 import com.blog.rest.api.payload.user.UserProfile;
 import com.blog.rest.api.payload.user.UserSummary;
+import com.blog.rest.api.repository.PostRepository;
+import com.blog.rest.api.repository.RoleRepository;
 import com.blog.rest.api.repository.UserRepository;
 import com.blog.rest.api.security.UserPrincipal;
 import com.blog.rest.api.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PostRepository postRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.postRepository = postRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -47,6 +57,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserProfile getUserProfile(String username) {
-        return null;
+        final User user = userRepository.getUserByName(username);
+        final Long postCount = postRepository.countByCreatedBy(user.getId());
+        return UserProfile.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .joinedAt(user.getCreatedAt())
+                .email(user.getEmail())
+                .address(user.getAddress())
+                .phone(user.getPhone())
+                .postCount(postCount)
+                .build();
     }
 }
