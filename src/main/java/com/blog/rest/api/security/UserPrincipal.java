ad087151ad34a1f3e5a9957b927e3c8acee6aa80
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-17 15:36:34 +0700

    updated property class UserPrincipal

--- src/main/java/com/blog/rest/api/security/UserPrincipal.java
+++ src/main/java/com/blog/rest/api/security/UserPrincipal.java
@@ -1,16 +1,21 @@
 package com.blog.rest.api.security;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serial;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 // class UserPrincipal ini implement interface UserDetail
 // yang artinya class ini bisa mengakses data detail dari User
+@Data
 public class UserPrincipal implements UserDetails {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private Long id;
@@ -51,36 +56,59 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        // cek apakah authorities bernilai null, jika true maka bernilai null
+        // jika false artinya ada authorities nya
+        return authorities == null ? null : new ArrayList<>(authorities);
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+        if (object == null || getClass() != object.getClass())
+            return false;
+        UserPrincipal that = (UserPrincipal) object;
+        return Objects.equals(id, that.id);
+    }
+
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }
