603a77230c0c65774bdbc436cb68181fb5f0c748
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-17 15:34:15 +0700

    added annotation @Builder

--- src/main/java/com/blog/rest/api/payload/user/UserSummary.java
+++ src/main/java/com/blog/rest/api/payload/user/UserSummary.java
@@ -1,11 +1,13 @@
 package com.blog.rest.api.payload.user;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 // class payload ini berisi data atau property User
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserSummary {
