80f1f443a987350da67ffe1c8636ad89e55eae14
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-17 16:22:47 +0700

    add annotation @Builder

--- src/main/java/com/blog/rest/api/payload/response/ApiResponse.java
+++ src/main/java/com/blog/rest/api/payload/response/ApiResponse.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import java.io.Serial;
 import java.io.Serializable;
 
 @Data
+@Builder
 @JsonPropertyOrder({
         "success",
         "message"
