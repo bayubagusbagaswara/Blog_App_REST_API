4da288ff5507cd6676b5db493910ff00e25f8a66
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-17 15:44:59 +0700

    added annotation @Builder

--- src/main/java/com/blog/rest/api/payload/user/UserIdentityAvailability.java
+++ src/main/java/com/blog/rest/api/payload/user/UserIdentityAvailability.java
@@ -1,9 +1,11 @@
 package com.blog.rest.api.payload.user;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class UserIdentityAvailability {
 
