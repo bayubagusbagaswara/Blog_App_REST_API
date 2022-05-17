76444a5bfa27acacd4d555e80235a7c092c1e25c
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-17 08:36:35 +0700

    added payload class UserIdentityAvailability

---
+++ src/main/java/com/blog/rest/api/payload/user/UserIdentityAvailability.java
@@ -0,0 +1,11 @@
+package com.blog.rest.api.payload.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserIdentityAvailability {
+
+    private Boolean available;
+}
