8d1f826f29ed117de0c78e0f99182907a9b87dd4
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-17 15:27:11 +0700

    added service implementation UserServiceImpl

---
+++ src/main/java/com/blog/rest/api/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.blog.rest.api.service.impl;
+
+import com.blog.rest.api.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
