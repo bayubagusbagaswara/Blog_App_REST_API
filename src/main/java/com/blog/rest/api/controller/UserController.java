a1557960b1eb8f01c8d5060e28e8732855bf1394
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-18 13:49:52 +0700

    added api method getUserProfile

--- src/main/java/com/blog/rest/api/controller/UserController.java
+++ src/main/java/com/blog/rest/api/controller/UserController.java
@@ -1,16 +1,14 @@
 package com.blog.rest.api.controller;
 
 import com.blog.rest.api.payload.user.UserIdentityAvailability;
+import com.blog.rest.api.payload.user.UserProfile;
 import com.blog.rest.api.payload.user.UserSummary;
 import com.blog.rest.api.security.CurrentUser;
 import com.blog.rest.api.security.UserPrincipal;
 import com.blog.rest.api.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -38,5 +36,11 @@ public class UserController {
     public ResponseEntity<UserIdentityAvailability> checkEmailAvailability(@RequestParam(value = "email") String email) {
         final UserIdentityAvailability userIdentityAvailability = userService.checkEmailAvailability(email);
         return new ResponseEntity<>(userIdentityAvailability, HttpStatus.OK);
+    }
+
+    @GetMapping("/{username}/profile")
+    public ResponseEntity<UserProfile> getUserProfile(@PathVariable(value = "username") String username) {
+        final UserProfile userProfile = userService.getUserProfile(username);
+        return new ResponseEntity<>(userProfile, HttpStatus.OK);
     }
 }
