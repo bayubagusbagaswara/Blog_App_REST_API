be086ad7f8331c3fc9812c6641d8d950e6fe26fc
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-18 16:28:55 +0700

    add implementation service AuthServiceImpl

---
+++ src/main/java/com/blog/rest/api/service/impl/AuthServiceImpl.java
@@ -0,0 +1,8 @@
+package com.blog.rest.api.service.impl;
+
+import com.blog.rest.api.service.AuthService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+}
