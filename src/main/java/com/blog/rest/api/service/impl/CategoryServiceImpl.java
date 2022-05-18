23cf6db7484eede58fbec2463be975fc2c998ce4
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-18 17:01:29 +0700

    added class implementation service CategoryServiceImpl

---
+++ src/main/java/com/blog/rest/api/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.blog.rest.api.service.impl;
+
+import com.blog.rest.api.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+}
