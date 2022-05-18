711d6ffa3be46c0cb0b0293dbf47044a3224b8f2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-18 17:10:13 +0700

    added method contract getCategoryById, addCategory, updateCategory, deleteCategory

--- src/main/java/com/blog/rest/api/service/CategoryService.java
+++ src/main/java/com/blog/rest/api/service/CategoryService.java
@@ -1,8 +1,19 @@
 package com.blog.rest.api.service;
 
 import com.blog.rest.api.entity.Category;
+import com.blog.rest.api.exception.UnauthorizedException;
+import com.blog.rest.api.payload.response.ApiResponse;
+import com.blog.rest.api.security.UserPrincipal;
 
 public interface CategoryService {
 
     Category getAllCategories(int page, int size);
+
+    Category getCategoryById(Long id);
+
+    Category addCategory(Category category, UserPrincipal currentUser);
+
+    Category updateCategory(Long id, Category newCategory, UserPrincipal userPrincipal) throws UnauthorizedException;
+
+    ApiResponse deleteCategory(Long id, UserPrincipal currentUser) throws UnauthorizedException;
 }
