34509fb24038f58dfd492903d48ffe77e506409c
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-20 20:49:10 +0700

    added api method addCategory

--- src/main/java/com/blog/rest/api/controller/CategoryController.java
+++ src/main/java/com/blog/rest/api/controller/CategoryController.java
@@ -1,8 +1,17 @@
 package com.blog.rest.api.controller;
 
+import com.blog.rest.api.entity.Category;
+import com.blog.rest.api.security.CurrentUser;
+import com.blog.rest.api.security.UserPrincipal;
 import com.blog.rest.api.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -12,5 +21,15 @@ public class CategoryController {
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    // add category
+    @PostMapping
+    public ResponseEntity<Category> addCategory(
+            @Valid @RequestBody Category category,
+            @CurrentUser UserPrincipal currentUser) {
+
+        final Category newCategory = categoryService.addCategory(category, currentUser);
+        return new ResponseEntity<>(newCategory, HttpStatus.CREATED);
     }
 }
