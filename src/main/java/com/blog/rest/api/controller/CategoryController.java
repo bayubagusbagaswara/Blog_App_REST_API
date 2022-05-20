8e258b659050da814cc72206117cc1d034078990
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-20 20:45:47 +0700

    added controller class CategoryController

---
+++ src/main/java/com/blog/rest/api/controller/CategoryController.java
@@ -0,0 +1,16 @@
+package com.blog.rest.api.controller;
+
+import com.blog.rest.api.service.CategoryService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+}
