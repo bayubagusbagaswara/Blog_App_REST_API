c09cc835f5cb9afc7353682ca987c3dc097e4170
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-21 11:20:27 +0700

    add method contract addAlbum

--- src/main/java/com/blog/rest/api/service/AlbumService.java
+++ src/main/java/com/blog/rest/api/service/AlbumService.java
@@ -1,5 +1,11 @@
 package com.blog.rest.api.service;
 
+import com.blog.rest.api.entity.Album;
+import com.blog.rest.api.payload.request.AlbumRequest;
+import com.blog.rest.api.security.UserPrincipal;
+
 public interface AlbumService {
+
+    Album addAlbum(AlbumRequest albumRequest, UserPrincipal currentUser);
 
 }
