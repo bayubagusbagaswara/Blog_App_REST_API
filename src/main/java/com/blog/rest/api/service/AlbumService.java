f53cb79625847ab040a9c9cec1c16780e164d2ae
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-21 11:27:07 +0700

    add method contract for Album service

--- src/main/java/com/blog/rest/api/service/AlbumService.java
+++ src/main/java/com/blog/rest/api/service/AlbumService.java
@@ -2,10 +2,22 @@ package com.blog.rest.api.service;
 
 import com.blog.rest.api.entity.Album;
 import com.blog.rest.api.payload.request.AlbumRequest;
+import com.blog.rest.api.payload.response.AlbumResponse;
+import com.blog.rest.api.payload.response.ApiResponse;
+import com.blog.rest.api.payload.response.PagedResponse;
 import com.blog.rest.api.security.UserPrincipal;
 
 public interface AlbumService {
 
     Album addAlbum(AlbumRequest albumRequest, UserPrincipal currentUser);
 
+    Album getAlbumById(Long id);
+
+    PagedResponse<AlbumResponse> getAllAlbums(int page, int size);
+
+    AlbumResponse updateAlbum(Long id, AlbumRequest newAlbum, UserPrincipal currentUser);
+
+    ApiResponse deleteAlbum(Long id, UserPrincipal currentUser);
+
+    Album getUserAlbums(String username, int page, int size);
 }
