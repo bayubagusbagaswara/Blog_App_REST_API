3f20cee73d0dd9de9c8f335bb64317dec94e6a80
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-22 13:29:01 +0700

    update entity Tag

--- src/main/java/com/blog/rest/api/entity/Tag.java
+++ src/main/java/com/blog/rest/api/entity/Tag.java
@@ -6,7 +6,17 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.io.Serial;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,28 +40,29 @@ public class Tag extends UserDateAudit {
     private String name;
 
     // buat table briging antara Tag dan Post, karena relasinya adalah Many To Many
-//    @JsonIgnore
-//    @ManyToMany(fetch = FetchType.EAGER)
-//    @JoinTable(
-//            name = "post_tag",
-//            joinColumns = @JoinColumn(name = "tag_id", foreignKey = @ForeignKey(name = "fk_post_tag_tag_id"), referencedColumnName = "id"),
-//            inverseJoinColumns = @JoinColumn(name = "post_id", foreignKey = @ForeignKey(name = "fk_post_tag_post_id"), referencedColumnName = "id"))
-//    private List<Post> posts = new ArrayList<>();
-//
-//    public Tag(String name) {
-//        super();
-//        this.name = name;
-//    }
-//
-//    public List<Post> getPosts() {
-//        return posts == null ? null : new ArrayList<>(posts);
-//    }
-//
-//    public void setPosts(List<Post> posts) {
-//        if (posts == null) {
-//            this.posts = null;
-//        } else {
-//            this.posts = Collections.unmodifiableList(posts);
-//        }
-//    }
+    @JsonIgnore
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "post_tag",
+            joinColumns = @JoinColumn(name = "tag_id", foreignKey = @ForeignKey(name = "fk_post_tag_tag_id"), referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "post_id", foreignKey = @ForeignKey(name = "fk_post_tag_post_id"), referencedColumnName = "id"))
+    private List<Post> posts = new ArrayList<>();
+
+    // Constructor with parameter is name tag
+    public Tag(String name) {
+        super();
+        this.name = name;
+    }
+
+    public List<Post> getPosts() {
+        return posts == null ? null : new ArrayList<>(posts);
+    }
+
+    public void setPosts(List<Post> posts) {
+        if (posts == null) {
+            this.posts = null;
+        } else {
+            this.posts = Collections.unmodifiableList(posts);
+        }
+    }
 }
