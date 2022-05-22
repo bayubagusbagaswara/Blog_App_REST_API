9bbe2f413674dc123c5aedb1cd04754c2893eb7c
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-22 15:23:19 +0700

    added method contract service addTag in the TagService

--- src/main/java/com/blog/rest/api/service/TagService.java
+++ src/main/java/com/blog/rest/api/service/TagService.java
@@ -1,9 +1,10 @@
 package com.blog.rest.api.service;
 
 import com.blog.rest.api.entity.Tag;
-
-import java.nio.file.attribute.UserPrincipal;
+import com.blog.rest.api.security.UserPrincipal;
 
 public interface TagService {
+
+    Tag addTag(Tag tag, UserPrincipal currentUser);
 
 }
