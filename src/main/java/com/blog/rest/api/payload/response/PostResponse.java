ea3cbd4859ae99f946b466da2adcfc0b9459d8bb
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-22 09:41:57 +0700

    added payload class PostResponse

---
+++ src/main/java/com/blog/rest/api/payload/response/PostResponse.java
@@ -0,0 +1,36 @@
+package com.blog.rest.api.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostResponse {
+
+    private Long id;
+    private String title;
+    private String body;
+    private String category;
+    private List<String> tags;
+
+    public List<String> getTags() {
+        return tags == null ? null : new ArrayList<>(tags);
+    }
+
+    public void setTags(List<String> tags) {
+        if (tags == null) {
+            this.tags = null;
+        } else {
+            this.tags = Collections.unmodifiableList(tags);
+        }
+    }
+
+}
