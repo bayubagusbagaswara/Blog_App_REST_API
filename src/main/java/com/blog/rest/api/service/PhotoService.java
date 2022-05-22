be0137bf890a9638cca491afc162f8e0e6f9c0df
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-22 20:25:37 +0700

    added method contract service is addPhoto in PhotoService

--- src/main/java/com/blog/rest/api/service/PhotoService.java
+++ src/main/java/com/blog/rest/api/service/PhotoService.java
@@ -1,5 +1,10 @@
 package com.blog.rest.api.service;
 
+import com.blog.rest.api.payload.photo.PhotoRequest;
+import com.blog.rest.api.payload.photo.PhotoResponse;
+import com.blog.rest.api.security.UserPrincipal;
+
 public interface PhotoService {
 
+    PhotoResponse addPhoto(PhotoRequest photoRequest, UserPrincipal currentUser);
 }
