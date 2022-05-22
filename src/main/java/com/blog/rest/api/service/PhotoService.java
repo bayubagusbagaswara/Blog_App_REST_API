bfcd97aa2a84e43c106a84333a0265a901ad3e66
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-22 20:27:14 +0700

    added method contract service is getAllPhotos in PhotoService

--- src/main/java/com/blog/rest/api/service/PhotoService.java
+++ src/main/java/com/blog/rest/api/service/PhotoService.java
@@ -2,6 +2,7 @@ package com.blog.rest.api.service;
 
 import com.blog.rest.api.payload.photo.PhotoRequest;
 import com.blog.rest.api.payload.photo.PhotoResponse;
+import com.blog.rest.api.payload.response.PagedResponse;
 import com.blog.rest.api.security.UserPrincipal;
 
 public interface PhotoService {
@@ -9,4 +10,6 @@ public interface PhotoService {
     PhotoResponse addPhoto(PhotoRequest photoRequest, UserPrincipal currentUser);
 
     PhotoResponse getPhotoById(Long id);
+
+    PagedResponse<PhotoResponse> getAllPhotos(int page, int size);
 }
