3e0581703eae7988abbbc1fa21480bd9f302d6dd
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-22 15:47:17 +0700

    added exception class is ResponseEntityErrorException

---
+++ src/main/java/com/blog/rest/api/exception/ResponseEntityErrorException.java
@@ -0,0 +1,22 @@
+package com.blog.rest.api.exception;
+
+import com.blog.rest.api.payload.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+
+import java.io.Serial;
+
+public class ResponseEntityErrorException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = -3156815846745801694L;
+
+    private final transient ResponseEntity<ApiResponse> apiResponse;
+
+    public ResponseEntityErrorException(ResponseEntity<ApiResponse> apiResponse) {
+        this.apiResponse = apiResponse;
+    }
+
+    public ResponseEntity<ApiResponse> getApiResponse() {
+        return apiResponse;
+    }
+}
