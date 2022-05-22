7778f302aa67af002bc30547970ad0c5b31ab41e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-22 09:47:10 +0700

    added payload class PostRequest

---
+++ src/main/java/com/blog/rest/api/payload/request/PostRequest.java
@@ -0,0 +1,45 @@
+package com.blog.rest.api.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostRequest {
+
+    @NotBlank
+    @Size(min = 10)
+    private String title;
+
+    @NotBlank
+    @Size(min = 50)
+    private String body;
+
+    @NotNull
+    private Long categoryId;
+
+    private List<String> tags;
+
+    public List<String> getTags() {
+        return tags == null ? Collections.emptyList() : new ArrayList<>(tags);
+    }
+
+    public void setTags(List<String> tags) {
+        if (tags == null) {
+            this.tags = null;
+        } else {
+            this.tags = Collections.unmodifiableList(tags);
+        }
+    }
+}
