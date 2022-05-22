68a3707a9031a07842d5758579551f120618fd03
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-22 20:48:48 +0700

    implementation logic method addPhoto in PhotoServiceImpl

--- src/main/java/com/blog/rest/api/service/impl/PhotoServiceImpl.java
+++ src/main/java/com/blog/rest/api/service/impl/PhotoServiceImpl.java
@@ -1,19 +1,66 @@
 package com.blog.rest.api.service.impl;
 
+import com.blog.rest.api.entity.Album;
+import com.blog.rest.api.entity.Photo;
+import com.blog.rest.api.exception.ResourceNotFoundException;
+import com.blog.rest.api.exception.UnauthorizedException;
 import com.blog.rest.api.payload.photo.PhotoRequest;
 import com.blog.rest.api.payload.photo.PhotoResponse;
 import com.blog.rest.api.payload.response.ApiResponse;
 import com.blog.rest.api.payload.response.PagedResponse;
+import com.blog.rest.api.repository.AlbumRepository;
+import com.blog.rest.api.repository.PhotoRepository;
 import com.blog.rest.api.security.UserPrincipal;
 import com.blog.rest.api.service.PhotoService;
 import org.springframework.stereotype.Service;
 
+import static com.blog.rest.api.utils.AppConstants.ALBUM;
+import static com.blog.rest.api.utils.AppConstants.ID;
+
 @Service
 public class PhotoServiceImpl implements PhotoService {
 
+    private final PhotoRepository photoRepository;
+    private final AlbumRepository albumRepository;
+
+    public PhotoServiceImpl(PhotoRepository photoRepository, AlbumRepository albumRepository) {
+        this.photoRepository = photoRepository;
+        this.albumRepository = albumRepository;
+    }
+
     @Override
     public PhotoResponse addPhoto(PhotoRequest photoRequest, UserPrincipal currentUser) {
-        return null;
+
+        // cek album
+        Album album = albumRepository.findById(photoRequest.getAlbumId())
+                .orElseThrow(() -> new ResourceNotFoundException(ALBUM, ID, photoRequest.getAlbumId()));
+
+        // cek user by id apakah sama
+        if (album.getUser().getId().equals(currentUser.getId())) {
+
+            // create photo
+            Photo photo = Photo.builder()
+                    .title(photoRequest.getTitle())
+                    .url(photoRequest.getUrl())
+                    .thumbnailUrl(photoRequest.getThumbnailUrl())
+                    .album(album)
+                    .build();
+
+            // save photo
+            Photo newPhoto = photoRepository.save(photo);
+
+            return PhotoResponse.builder()
+                    .id(newPhoto.getId())
+                    .title(newPhoto.getTitle())
+                    .url(newPhoto.getUrl())
+                    .thumbnailUrl(newPhoto.getThumbnailUrl())
+                    .albumId(newPhoto.getAlbum().getId())
+                    .build();
+        }
+
+        // balikan jika tidak bisa menambahkan photo
+        ApiResponse apiResponse = new ApiResponse(Boolean.FALSE, "You don't have permission to add photo in this album");
+        throw new UnauthorizedException(apiResponse);
     }
 
     @Override
