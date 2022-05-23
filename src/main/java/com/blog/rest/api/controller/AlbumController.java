a23daffb50557af84a78cb6047566f976ba696a9
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 10:27:31 +0700

    added method handler getAllPhotosByAlbum in AlbumController

--- src/main/java/com/blog/rest/api/controller/AlbumController.java
+++ src/main/java/com/blog/rest/api/controller/AlbumController.java
@@ -6,6 +6,7 @@ import com.blog.rest.api.payload.ApiResponse;
 import com.blog.rest.api.payload.PagedResponse;
 import com.blog.rest.api.payload.album.AlbumRequest;
 import com.blog.rest.api.payload.album.AlbumResponse;
+import com.blog.rest.api.payload.photo.PhotoResponse;
 import com.blog.rest.api.security.CurrentUser;
 import com.blog.rest.api.security.UserPrincipal;
 import com.blog.rest.api.service.AlbumService;
@@ -82,6 +83,16 @@ public class AlbumController {
 
         final ApiResponse apiResponse = albumService.deleteAlbum(id, currentUser);
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/photos")
+    public ResponseEntity<PagedResponse<PhotoResponse>> getAllPhotosByAlbum(
+            @PathVariable(name = "id") Long id,
+            @RequestParam(name = "page", required = false, defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) Integer page,
+            @RequestParam(name = "size", required = false, defaultValue = AppConstants.DEFAULT_PAGE_SIZE) Integer size) {
+
+        PagedResponse<PhotoResponse> response = photoService.getAllPhotosByAlbum(id, page, size);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
