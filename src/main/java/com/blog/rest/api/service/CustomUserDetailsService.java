2e2dfc336250838221094fd329d4c2fb925816f4
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 10:09:42 +0700

    extends interface UserDetailsService

--- src/main/java/com/blog/rest/api/service/CustomUserDetailsService.java
+++ src/main/java/com/blog/rest/api/service/CustomUserDetailsService.java
@@ -1,8 +1,9 @@
 package com.blog.rest.api.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface CustomUserDetailsService {
+public interface CustomUserDetailsService extends UserDetailsService {
 
     // ambil data UserDetails by id
     UserDetails loadUserById(Long id);
