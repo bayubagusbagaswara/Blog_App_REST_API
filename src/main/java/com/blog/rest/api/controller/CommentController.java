80c9268383c32b990aefe4abb1578f95e6908aba
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 11:05:37 +0700

    add method handler addComment in CommentController

--- src/main/java/com/blog/rest/api/controller/CommentController.java
+++ src/main/java/com/blog/rest/api/controller/CommentController.java
@@ -1,9 +1,35 @@
 package com.blog.rest.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.blog.rest.api.entity.Comment;
+import com.blog.rest.api.payload.comment.CommentRequest;
+import com.blog.rest.api.security.CurrentUser;
+import com.blog.rest.api.security.UserPrincipal;
+import com.blog.rest.api.service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/posts/{postId}/comments")
 public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+//    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Comment> addComment(
+            @Valid @RequestBody CommentRequest commentRequest,
+            @PathVariable(name = "postId") Long postId,
+            @CurrentUser UserPrincipal currentUser) {
+
+        Comment newComment = commentService.addComment(commentRequest, postId, currentUser);
+        return new ResponseEntity<>(newComment, HttpStatus.CREATED);
+    }
 }
