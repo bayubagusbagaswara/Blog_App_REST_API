183c0323bb8cb5b5718366680d355664320435c3
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 12:35:03 +0700

    added method handler addTag in TagController

--- src/main/java/com/blog/rest/api/controller/TagController.java
+++ src/main/java/com/blog/rest/api/controller/TagController.java
@@ -1,10 +1,36 @@
 package com.blog.rest.api.controller;
 
+import com.blog.rest.api.entity.Tag;
+import com.blog.rest.api.security.CurrentUser;
+import com.blog.rest.api.security.UserPrincipal;
+import com.blog.rest.api.service.TagService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/tags")
 public class TagController {
 
+    private final TagService tagService;
+
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @PostMapping
+//    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Tag> addTag(
+            @Valid @RequestBody Tag tag,
+            @CurrentUser UserPrincipal currentUser) {
+
+        Tag newTag = tagService.addTag(tag, currentUser);
+        return new ResponseEntity<>(newTag, HttpStatus.CREATED);
+    }
 }
