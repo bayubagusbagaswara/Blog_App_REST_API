e7bae36ae9f3ea073f7f5231ede70a4ea10c3a03
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 10:05:58 +0700

    implementation method loadUserById

--- src/main/java/com/blog/rest/api/service/impl/CustomUserDetailsServiceImpl.java
+++ src/main/java/com/blog/rest/api/service/impl/CustomUserDetailsServiceImpl.java
@@ -1,17 +1,31 @@
 package com.blog.rest.api.service.impl;
 
+import com.blog.rest.api.entity.user.User;
+import com.blog.rest.api.repository.UserRepository;
+import com.blog.rest.api.security.UserPrincipal;
 import com.blog.rest.api.service.CustomUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class CustomUserDetailsServiceImpl implements UserDetailsService, CustomUserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserById(Long id) {
-        return null;
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User not found with id: %s", id)));
+
+        return UserPrincipal.create(user);
     }
 
     @Override
