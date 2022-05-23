e539547c4c9e812e5cb50de2ebbcbd95381b957a
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 12:33:02 +0700

    added class TagController

---
+++ src/main/java/com/blog/rest/api/controller/TagController.java
@@ -0,0 +1,10 @@
+package com.blog.rest.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tags")
+public class TagController {
+
+}
