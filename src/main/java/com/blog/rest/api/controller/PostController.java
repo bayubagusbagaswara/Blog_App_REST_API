a24b84a6ca914d2475553137a5855843906ba630
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 11:47:58 +0700

    added method handler api getPostById in PostController

--- src/main/java/com/blog/rest/api/controller/PostController.java
+++ src/main/java/com/blog/rest/api/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.blog.rest.api.controller;
 
+import com.blog.rest.api.entity.Post;
 import com.blog.rest.api.payload.post.PostRequest;
 import com.blog.rest.api.payload.post.PostResponse;
 import com.blog.rest.api.security.CurrentUser;
@@ -8,10 +9,7 @@ import com.blog.rest.api.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -34,4 +32,11 @@ public class PostController {
         PostResponse postResponse = postService.addPost(postRequest, currentUser);
         return new ResponseEntity<>(postResponse, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable(name = "id") Long id) {
+        Post post = postService.getPostById(id);
+        return new ResponseEntity<>(post, HttpStatus.OK);
+    }
+
 }
