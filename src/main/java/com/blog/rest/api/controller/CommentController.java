e47c65d52b8d25b9cd3cdd467aa9a734515cfd63
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 10:28:51 +0700

    add class controller CommentController

---
+++ src/main/java/com/blog/rest/api/controller/CommentController.java
@@ -0,0 +1,9 @@
+package com.blog.rest.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts/{postId}/comments")
+public class CommentController {
+}
