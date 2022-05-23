96ddc3674036deaa1a1edc635191be40fef81d92
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 11:13:01 +0700

    added method handler addPhoto in PhotoController

--- src/main/java/com/blog/rest/api/controller/PhotoController.java
+++ src/main/java/com/blog/rest/api/controller/PhotoController.java
@@ -1,8 +1,19 @@
 package com.blog.rest.api.controller;
 
+import com.blog.rest.api.payload.photo.PhotoRequest;
+import com.blog.rest.api.payload.photo.PhotoResponse;
+import com.blog.rest.api.security.CurrentUser;
+import com.blog.rest.api.security.UserPrincipal;
 import com.blog.rest.api.service.PhotoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/photos")
@@ -13,4 +24,15 @@ public class PhotoController {
     public PhotoController(PhotoService photoService) {
         this.photoService = photoService;
     }
+
+    @PostMapping
+//    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<PhotoResponse> addPhoto(
+            @Valid @RequestBody PhotoRequest photoRequest,
+            @CurrentUser UserPrincipal currentUser) {
+
+        PhotoResponse photoResponse = photoService.addPhoto(photoRequest, currentUser);
+        return new ResponseEntity<>(photoResponse, HttpStatus.OK);
+    }
+
 }
