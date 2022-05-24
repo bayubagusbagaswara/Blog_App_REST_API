3edba33e001901566187b0c980d7a985c1797487
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-24 11:23:16 +0700

    added method registerUser

--- src/main/java/com/blog/rest/api/controller/AuthController.java
+++ src/main/java/com/blog/rest/api/controller/AuthController.java
@@ -1,10 +1,18 @@
 package com.blog.rest.api.controller;
 
+import com.blog.rest.api.entity.role.Role;
+import com.blog.rest.api.entity.role.RoleName;
+import com.blog.rest.api.entity.user.User;
+import com.blog.rest.api.exception.AppException;
+import com.blog.rest.api.exception.BlogApiException;
+import com.blog.rest.api.payload.ApiResponse;
 import com.blog.rest.api.payload.auth.LoginRequest;
+import com.blog.rest.api.payload.auth.SignUpRequest;
 import com.blog.rest.api.payload.jwt.JwtAuthenticationResponse;
 import com.blog.rest.api.repository.RoleRepository;
 import com.blog.rest.api.repository.UserRepository;
 import com.blog.rest.api.security.JwtTokenProvider;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,8 +23,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -57,5 +69,48 @@ public class AuthController {
         // generate token jwt
         final String jwt = jwtTokenProvider.generateToken(authentication);
         return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+        if (Boolean.TRUE.equals(userRepository.existsByUsername(signUpRequest.getUsername()))) {
+            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Username is already taken");
+        }
+
+        if (Boolean.TRUE.equals(userRepository.existsByEmail(signUpRequest.getEmail()))) {
+            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Email is already taken");
+        }
+
+        String firstName = signUpRequest.getFirstName().toLowerCase();
+        String lastName = signUpRequest.getLastName().toLowerCase();
+        String username = signUpRequest.getUsername().toLowerCase();
+        String email = signUpRequest.getEmail().toLowerCase();
+        String password = passwordEncoder.encode(signUpRequest.getPassword());
+
+        // create object user
+        User user = new User(firstName, lastName, username, email, password);
+
+        // create object role
+        List<Role> roles = new ArrayList<>();
+
+        if (userRepository.count() == 0) {
+            roles.add(roleRepository.findByName(RoleName.ROLE_USER)
+                    .orElseThrow(() -> new AppException(USER_ROLE_NOT_SET)));
+            roles.add(roleRepository.findByName(RoleName.ROLE_ADMIN)
+                    .orElseThrow(() -> new AppException(USER_ROLE_NOT_SET)));
+        } else {
+            roles.add(roleRepository.findByName(RoleName.ROLE_USER)
+                    .orElseThrow(() -> new AppException(USER_ROLE_NOT_SET)));
+        }
+
+        user.setRoles(roles);
+
+        User result = userRepository.save(user);
+
+        // arahkan ke url /api/users/{id}
+        URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users/{userId}")
+                .buildAndExpand(result.getId()).toUri();
+
+        return ResponseEntity.created(location).body(new ApiResponse(Boolean.TRUE, "User registered successfully"));
     }
 }
