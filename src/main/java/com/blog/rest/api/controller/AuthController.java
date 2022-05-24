d30f40d1f1329a9106b773c8cee15dfd7c7bdd64
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-24 11:20:25 +0700

    added method authenticateUser

--- src/main/java/com/blog/rest/api/controller/AuthController.java
+++ src/main/java/com/blog/rest/api/controller/AuthController.java
@@ -1,9 +1,61 @@
 package com.blog.rest.api.controller;
 
+import com.blog.rest.api.payload.auth.LoginRequest;
+import com.blog.rest.api.payload.jwt.JwtAuthenticationResponse;
+import com.blog.rest.api.repository.RoleRepository;
+import com.blog.rest.api.repository.UserRepository;
+import com.blog.rest.api.security.JwtTokenProvider;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    private static final String USER_ROLE_NOT_SET = "User role not set";
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    // saat pertama kali sign in, maka user akan memasukkan username/email dan password
+    // setelah berhasil terautentiaksi, maka user akan mendapatkan Token
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthenticationResponse> authenticateUser(
+            @Valid @RequestBody LoginRequest loginRequest) {
+
+        // untuk autentikasi kita cukup menggunakan authenticationManager
+        // autentikasi berdasarkan username dan password, apakah cocok dengan data di database
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsernameOrEmail(),
+                        loginRequest.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // generate token jwt
+        final String jwt = jwtTokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
 }
