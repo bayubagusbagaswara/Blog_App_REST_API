4315720ecab2fcb134fd739fde23ef904f4fc4b4
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-25 12:38:15 +0700

    implement method signIn

--- src/main/java/com/blog/rest/api/service/impl/AuthServiceImpl.java
+++ src/main/java/com/blog/rest/api/service/impl/AuthServiceImpl.java
@@ -1,8 +1,52 @@
 package com.blog.rest.api.service.impl;
 
+import com.blog.rest.api.payload.auth.LoginRequest;
+import com.blog.rest.api.payload.jwt.JwtAuthenticationResponse;
+import com.blog.rest.api.repository.RoleRepository;
+import com.blog.rest.api.repository.UserRepository;
+import com.blog.rest.api.security.JwtTokenProvider;
 import com.blog.rest.api.service.AuthService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
+
+    private static final String USER_ROLE_NOT_SET = "User role not set";
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthServiceImpl(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public JwtAuthenticationResponse signIn(LoginRequest loginRequest) {
+
+        // untuk autentikasi kita cukup menggunakan authenticationManager
+        // autentikasi berdasarkan username dan password, apakah cocok dengan data di database
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsernameOrEmail(),
+                        loginRequest.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // generate token jwt
+        final String jwt = jwtTokenProvider.generateToken(authentication);
+        return new JwtAuthenticationResponse(jwt);
+    }
 }
