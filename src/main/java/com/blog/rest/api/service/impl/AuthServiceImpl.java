c107121c959805efcf2032b11ae669f28469228d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-25 12:40:04 +0700

    implement method signUp

--- src/main/java/com/blog/rest/api/service/impl/AuthServiceImpl.java
+++ src/main/java/com/blog/rest/api/service/impl/AuthServiceImpl.java
@@ -1,17 +1,27 @@
 package com.blog.rest.api.service.impl;
 
+import com.blog.rest.api.entity.role.Role;
+import com.blog.rest.api.entity.role.RoleName;
+import com.blog.rest.api.entity.user.User;
+import com.blog.rest.api.exception.AppException;
+import com.blog.rest.api.exception.BlogApiException;
 import com.blog.rest.api.payload.auth.LoginRequest;
+import com.blog.rest.api.payload.auth.SignUpRequest;
 import com.blog.rest.api.payload.jwt.JwtAuthenticationResponse;
 import com.blog.rest.api.repository.RoleRepository;
 import com.blog.rest.api.repository.UserRepository;
 import com.blog.rest.api.security.JwtTokenProvider;
 import com.blog.rest.api.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -48,5 +58,43 @@ public class AuthServiceImpl implements AuthService {
         // generate token jwt
         final String jwt = jwtTokenProvider.generateToken(authentication);
         return new JwtAuthenticationResponse(jwt);
+    }
+
+    @Override
+    public User signUp(SignUpRequest signUpRequest) {
+
+        if (Boolean.TRUE.equals(userRepository.existsByUsername(signUpRequest.getUsername()))) {
+            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Username is already taken");
+        }
+
+        if (Boolean.TRUE.equals(userRepository.existsByEmail(signUpRequest.getEmail()))) {
+            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Email is already taken");
+        }
+
+        String firstName = signUpRequest.getFirstName().toLowerCase();
+        String lastName = signUpRequest.getLastName().toLowerCase();
+        String username = signUpRequest.getUsername().toLowerCase();
+        String email = signUpRequest.getEmail().toLowerCase();
+        String password = passwordEncoder.encode(signUpRequest.getPassword());
+
+        // create object user
+        User user = new User(firstName, lastName, username, email, password);
+
+        // create object role
+        List<Role> roles = new ArrayList<>();
+
+        if (userRepository.count() == 0) {
+            roles.add(roleRepository.findByName(RoleName.ROLE_USER)
+                    .orElseThrow(() -> new AppException(USER_ROLE_NOT_SET)));
+            roles.add(roleRepository.findByName(RoleName.ROLE_ADMIN)
+                    .orElseThrow(() -> new AppException(USER_ROLE_NOT_SET)));
+        } else {
+            roles.add(roleRepository.findByName(RoleName.ROLE_USER)
+                    .orElseThrow(() -> new AppException(USER_ROLE_NOT_SET)));
+        }
+
+        user.setRoles(roles);
+
+        return userRepository.save(user);
     }
 }
