24f87300ac8ddc322180579e5f159bfe3b4c1101
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-25 12:34:55 +0700

    add method contract service signIn

--- src/main/java/com/blog/rest/api/service/AuthService.java
+++ src/main/java/com/blog/rest/api/service/AuthService.java
@@ -1,8 +1,12 @@
 package com.blog.rest.api.service;
 
+import com.blog.rest.api.payload.auth.LoginRequest;
+import com.blog.rest.api.payload.jwt.JwtAuthenticationResponse;
+
 public interface AuthService {
 
     // signin
+    JwtAuthenticationResponse signIn(LoginRequest loginRequest);
 
     // signup
 }
