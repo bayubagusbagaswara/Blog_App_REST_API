8040f48e26883b004aad50ca269d7994e4318a4d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-30 15:08:31 +0700

    add bean ModelMapper, method init timezone, and bean jwtAuthenticationFilter

--- src/main/java/com/blog/rest/api/SpringBootBlogWebAppRestApiApplication.java
+++ src/main/java/com/blog/rest/api/SpringBootBlogWebAppRestApiApplication.java
@@ -1,13 +1,36 @@
 package com.blog.rest.api;
 
+import com.blog.rest.api.security.JwtAuthenticationFilter;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.convert.Jsr310Converters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = { SpringBootBlogWebAppRestApiApplication.class, Jsr310Converters.class })
 public class SpringBootBlogWebAppRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBlogWebAppRestApiApplication.class, args);
 	}
 
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter();
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
