5bcfdf63a8f94af1709856a55dcdc394e045e32b
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-03 18:26:37 +0700

    update SpringSecurityAuditAwareImpl

--- src/main/java/com/blog/rest/api/config/SpringSecurityAuditAwareImpl.java
+++ src/main/java/com/blog/rest/api/config/SpringSecurityAuditAwareImpl.java
@@ -1,8 +1,8 @@
 package com.blog.rest.api.config;
 
 import com.blog.rest.api.security.UserPrincipal;
+import lombok.NonNull;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.lang.NonNullApi;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,20 +12,17 @@ import java.util.Optional;
 
 public class SpringSecurityAuditAwareImpl implements AuditorAware<String> {
 
+    @NonNull
     @Override
     public Optional<String> getCurrentAuditor() {
-
-        // ambil authentication
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        // cek jika authentication kosong, maka balikan data kosong
         if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
             return Optional.empty();
         }
 
-        // jika authentication ada datanya, maka ambil data dari UserPrincipal
-        UserPrincipal userPrincipal = (UserPrincipal) Objects.requireNonNull(authentication).getPrincipal();// balikan data username dari userPrincipal
+        UserPrincipal userPrincipal = (UserPrincipal) Objects.requireNonNull(authentication).getPrincipal();
 
-        return Optional.ofNullable(userPrincipal.getUsername());
+        return Optional.ofNullable(userPrincipal.getEmail());
     }
 }
