80f6442384c355a53f6d89c56cbb13b295583d16
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-03 18:26:23 +0700

    update AuditingConfig

--- src/main/java/com/blog/rest/api/config/AuditingConfig.java
+++ src/main/java/com/blog/rest/api/config/AuditingConfig.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class AuditingConfig {
 
-    // create bean for AuditorAware
     @Bean
     public AuditorAware<String> auditorProvider() {
         return new SpringSecurityAuditAwareImpl();
