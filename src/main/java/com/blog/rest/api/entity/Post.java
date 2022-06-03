2973822026fc954b1766623995103b7e1574eefe
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-03 19:19:02 +0700

    updated entity Post

--- src/main/java/com/blog/rest/api/entity/Post.java
+++ src/main/java/com/blog/rest/api/entity/Post.java
@@ -5,25 +5,38 @@ import com.blog.rest.api.entity.user.User;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.io.Serial;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
-@Table(name = "posts", uniqueConstraints = {
-        @UniqueConstraint(columnNames = { "title" }) })
+@Table(name = "posts", uniqueConstraints = @UniqueConstraint(name = "post_title_unique", columnNames = "title"))
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Post extends UserDateAudit {
-
-    @Serial
-    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -47,7 +60,6 @@ public class Post extends UserDateAudit {
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
 
-    // buat table briging
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "post_tag",
