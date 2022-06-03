47bc4a69240c06e83c67bc6db1aed213f7c799de
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-03 19:19:49 +0700

    updated entity Tag

--- src/main/java/com/blog/rest/api/entity/Tag.java
+++ src/main/java/com/blog/rest/api/entity/Tag.java
@@ -17,7 +17,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import java.io.Serial;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -28,10 +27,6 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class Tag extends UserDateAudit {
-
-    @Serial
-    private static final long serialVersionUID = -5298707266367331514L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -39,7 +34,6 @@ public class Tag extends UserDateAudit {
     @Column(name = "name")
     private String name;
 
-    // buat table briging antara Tag dan Post, karena relasinya adalah Many To Many
     @JsonIgnore
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
@@ -48,7 +42,6 @@ public class Tag extends UserDateAudit {
             inverseJoinColumns = @JoinColumn(name = "post_id", foreignKey = @ForeignKey(name = "fk_post_tag_post_id"), referencedColumnName = "id"))
     private List<Post> posts;
 
-    // Constructor with parameter is name tag
     public Tag(String name) {
         super();
         this.name = name;
