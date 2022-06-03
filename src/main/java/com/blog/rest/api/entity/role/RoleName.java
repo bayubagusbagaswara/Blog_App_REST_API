a3c4cf8026ed8b5a421a006b7469ffcaf677c74f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-03 18:56:25 +0700

    updated enum RoleName

--- src/main/java/com/blog/rest/api/entity/role/RoleName.java
+++ src/main/java/com/blog/rest/api/entity/role/RoleName.java
@@ -1,6 +1,17 @@
 package com.blog.rest.api.entity.role;
 
 public enum RoleName {
-    ROLE_ADMIN,
-    ROLE_USER,
+
+    ROLE_ADMIN("ROLE_ADMIN"),
+    ROLE_USER("ROLE_USER");
+
+    private final String roleName;
+
+    RoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
 }
