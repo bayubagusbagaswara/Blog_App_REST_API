e17fb42f137371121fb38dcac4a28ef0dcfc469f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-04 08:44:21 +0700

    update interface AlbumRepository

--- src/main/java/com/blog/rest/api/repository/AlbumRepository.java
+++ src/main/java/com/blog/rest/api/repository/AlbumRepository.java
@@ -8,7 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
-
-    // mencari album berdasarkan created by dari user tertentu
     Page<Album> findByCreatedBy(Long userId, Pageable pageable);
 }
