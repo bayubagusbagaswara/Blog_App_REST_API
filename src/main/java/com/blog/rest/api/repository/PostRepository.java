733ce5107a07a040e8066a1f6941109ec16ebb37
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-04 08:44:51 +0700

    update interface PostRepository

--- src/main/java/com/blog/rest/api/repository/PostRepository.java
+++ src/main/java/com/blog/rest/api/repository/PostRepository.java
@@ -12,15 +12,11 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    // mencari Post berdasarkan created by user tertentu
     Page<Post> findByCreatedBy(Long userId, Pageable pageable);
 
-    // mencari Post berdasarkan category
     Page<Post> findByCategory(Long categoryId, Pageable pageable);
 
-    // mencari Post berdasarkan tags, tags bisa lebih dari satu
 //    Page<Post> findByTags(List<Tag> tags, Pageable pageable);
 
-    // menghitung jumlah post berdasarkan user yang membuatnya
     Long countByCreatedBy(Long userId);
 }
